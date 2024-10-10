5abc3fb82cde24281a51698628ce03a5980f2bfb
Syawalridho <syawalridho3@gmail.com>
2024-10-10 18:18:38 +0700

    update

---
+++ Soal Asistensi P1/Jawaban 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int n[7], i;
+
+    scanf("hari : %i", &i);
+
+    if(i = 1)
+    {
+        printf("Xing qi yi");
+    }
+    else if(i = 2)
+    {
+        printf("Xing qi er");
+    }
+    
+    return 0;
+}
+
+
+
+
